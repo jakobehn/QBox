f015105bf069657c337e45f8dfc53925de3b7d4a
Jakob Ehn <jakob@ehn.nu>
2017-12-03 21:55:03 +0100

    added controller with a file reader

---
+++ Source/QBox.Web/Controllers/InfoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QBox.Web.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info
+        public ActionResult Index()
+        {
+            using (var sr = new StreamReader("c:\\temp\\info.txt"))
+            {
+                var text = sr.ReadToEnd();
+                Response.Write(text);
+                Response.End();
+            }
+            return View();
+        }
+    }
+}
