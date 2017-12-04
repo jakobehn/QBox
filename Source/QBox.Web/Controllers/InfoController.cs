185844cdd9526dbc11501ab22a26117ee6e37627
Jakob Ehn <jakob@ehn.nu>
2017-12-04 15:51:29 +0100

    adding controller that writes to file

--- Source/QBox.Web/Controllers/InfoController.cs
+++ Source/QBox.Web/Controllers/InfoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 
 namespace QBox.Web.Controllers
@@ -12,12 +13,30 @@ namespace QBox.Web.Controllers
         // GET: Info
         public ActionResult Index()
         {
-            using (var sr = new StreamReader("c:\\temp\\info.txt"))
+            int nr = 0;
+            var file = "c:\\temp\\data\\info.txt";
+            if (!System.IO.File.Exists(file))
             {
-                var text = sr.ReadToEnd();
-                Response.Write(text);
-                Response.End();
+                using (var sw = new StreamWriter(System.IO.File.Create(file)))
+                {
+                    sw.Write(0);
+                }
             }
+            else
+            {
+                using (var sr = new StreamReader(file))
+                {
+                    nr = Convert.ToInt32(sr.ReadToEnd());
+                }
+            }
+
+            using (var sw = new StreamWriter(file))
+            {
+                sw.Write(nr+1);
+            }
+
+            Response.Write(nr+1);
+                Response.End();
             return View();
         }
     }
